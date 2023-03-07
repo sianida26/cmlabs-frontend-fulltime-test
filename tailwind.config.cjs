e472115ab64dc95f083bc37ecf13b80e7099b30a
sianida26 <chesakeselekkebo@gmail.com>
2023-03-08 02:10:00 +0700

    Added TailwindCSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.emerald,
+      }
+    },
+  },
+  plugins: [],
+}
